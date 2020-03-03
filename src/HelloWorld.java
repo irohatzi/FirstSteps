2d259e61e6fa3900fa862e31383f599829b19565
Iro <iro.hatzi@gmail.com>
2020-03-03 11:16:15 +0100

    Wir geben mehrere Zeilen aus

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Campus");
+        // HelloWorld
+        System.out.println("Hallo Swengis");
+        System.out.println("was geht ab");
+        // TODO: muss HelloWorld beinhalten
+    }
+}
+
